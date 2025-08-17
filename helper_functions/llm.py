87c4efb45b8ef50b5fc059be6dceba9ba3bca4d0
bibibabibu-sg <nkxt82@gmail.com>
2025-08-17 19:22:02 +0800

    Bug fix

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -5,7 +5,7 @@ import tiktoken
 import streamlit as st
 #from langchain_chroma import Chroma
 from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+#from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 #from dotenv import load_dotenv  
